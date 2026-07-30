2085eed142cdfe4188541ad771d3ebb11ca8673b
Mariam Mohammed <mohammedmariam0820@gmail.com>
2026-07-30 11:08:09 +0100

    Add temperature adjustment based on drink type

---
+++ conditionals.py
@@ -0,0 +1,10 @@
+drink = "Coffee"
+temperature = 100
+
+if drink == "Green Tea":
+     temperature = 80 
+elif drink == "Coffee":
+     temperature = 90
+else:
+     temperature = 100
+print(temperature)
