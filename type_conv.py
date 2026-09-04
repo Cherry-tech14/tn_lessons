89438841128962f0546948c6727c0a1181343177
Cherry-tech14 <mohammedmariam0820@gmail.com>
2026-09-04 10:26:28 +0100

    tn lesson on type_casting

---
+++ type_conv.py
@@ -0,0 +1,48 @@
+'''
+age = "24"
+age = int(age)
+print(age)
+
+
+price = "5000"
+tax = 500
+print(int(price) + tax)
+
+
+# string parsing
+user_input = "24"
+value = int(user_input)
+print(value + 10)
+
+#handling diff numbers formats: Removing commas
+price_with_comma = "2,500.00"
+cleaned = price_with_comma.replace(",", "")
+value = float(cleaned)
+print(value)
+
+# Removing currency symbols
+price_with_currency = "#2500.00"
+cleaned = price_with_currency.replace("#", "")
+value = float(cleaned)
+print(value)
+
+# Handling whitespace
+price_with_spaces = " 2500.50 "
+cleaned = price_with_spaces.replace(" ", "")
+value = float(cleaned)
+print(value)
+'''
+
+#functions that handles multiple formats
+def parse_nigerian_number(input_string):
+    cleaned = input_string.replace(",", "")
+    cleaned = cleaned.strip()
+    try:
+        return float(cleaned)
+    except ValueError:
+            cleaned = cleaned.replace("", "")
+            return float(cleaned)
+            print(parse_nigerian_number("₦2,500.00"))  # 2500.0
+print(parse_nigerian_number("1,500.50"))   # 1500.5
+print(parse_nigerian_number("3000"))       # 3000.0
+print(parse_nigerian_number("  250  "))    # 250.0
